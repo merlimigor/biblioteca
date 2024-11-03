a25818755b7fa00341264c1bcf5723bf2f332d89
merlimigor <merlimigor@yahoo.com>
2024-11-02 21:06:18 -0300

    só falta o front2

--- livros/forms.py
+++ livros/forms.py
@@ -1,10 +1,15 @@
-# livros/forms.py
 from django import forms
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput, label="Senha")
 
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name', 'last_name', 'email']
+        labels = {
+            'username': "Nome de usuário",
+            'first_name': "Primeiro nome",
+            'last_name': "Último nome",
+            'email': "Endereço de e-mail",
+        }
